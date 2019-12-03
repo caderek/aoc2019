0f2951d8197cb9f066cfe878cd79b593eac50b57
Maciej Cąderek <maciej.caderek@gmail.com>
2019-12-03 21:26:16 +0100

    Added option to tun all solutions

--- scripts/start.js
+++ scripts/start.js
@@ -1,15 +1,25 @@
-const { spawn, execSync } = require("child_process")
+const { spawn, spawnSync } = require("child_process")
 const { readdirSync } = require("fs")
 const { cp } = require("shelljs")
 
 const day = process.argv[2]
 const days = readdirSync("./src")
 
-if (!days.includes(day)) {
+if (!days.includes(day) && day !== "all") {
   console.log(`Creating file structure for ${day}...`)
   cp("-r", "src/template", `src/${day}`)
 }
 
-spawn("nodemon", ["-x", "ts-node", `src/${day}/index.ts`], {
-  stdio: "inherit",
-})
+if (day === "all") {
+  days
+    .filter((folder) => !["template, utils"].includes(folder))
+    .forEach((day) => {
+      spawnSync("ts-node", [`src/${day}/index.ts`], {
+        stdio: "inherit",
+      })
+    })
+} else {
+  spawn("nodemon", ["-x", "ts-node", `src/${day}/index.ts`], {
+    stdio: "inherit",
+  })
+}
