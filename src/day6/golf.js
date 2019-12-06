f774dd1a6c6b11a63c9bcf270c5b6133c1a3f9b9
Maciej Cąderek <maciej.caderek@gmail.com>
2019-12-06 14:46:56 +0100

    Day 6 - golf

--- src/day6/golf.js
+++ src/day6/golf.js
@@ -1,4 +1,4 @@
 G=require("graphlib"),[require("fs").readFileSync(`./input.txt`).toString()
 .split("\n").reduce((g,x)=>g.setEdge(...x.split(")")),new G.Graph())].map(
-(g)=>console.log(Object.values(G.alg.dijkstra(g,"COM")).reduce((x,n)=>x+n
+g=>console.log(Object.values(G.alg.dijkstra(g,"COM")).reduce((x,n)=>x+n
 .distance,0),G.alg.dijkstra(g,"YOU",null,g.nodeEdges.bind(g)).SAN.distance-2))
