6e19df69a8a6ac1386bc8b5a08f0199ba5b9eb84
Maciej Cąderek <maciej.caderek@gmail.com>
2019-12-13 11:24:24 +0100

    Day 13 render

--- src/day13/index.ts
+++ src/day13/index.ts
@@ -1,11 +1,45 @@
+import logUpdate from "log-update"
+import * as kleur from "kleur"
 import { readInput, arr } from "../utils/index"
 import compute, { unblock } from "./computer"
 
 type State = [number, number, number][]
 
+const tiles = {
+  0: " ",
+  1: kleur.blue("H"),
+  2: kleur.green("#"),
+  3: kleur.red("–"),
+  4: kleur.yellow("●"),
+}
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+const renderer = (grid = null) => async (state: State, score = 0) => {
+  const w = Math.max(...state.map((tile) => tile[0])) + 1
+  const h = Math.max(...state.map((tile) => tile[1])) + 1
+
+  const board = grid
+    ? grid
+    : Array.from({ length: h }, () => Array.from({ length: w }, () => " "))
+
+  grid = board
+
+  state.forEach(([x, y, id]) => {
+    board[y][x] = tiles[id]
+  })
+
+  const screen = board.map((row) => row.join("")).join("\n")
+  logUpdate(`\nScore: ${score}\n${screen}\n`)
+  await delay(10)
+}
+
 const goA = async (source: string) => {
   const game = await compute(source)
   const state = arr.chunk_(3, game) as State
+
+  /* Uncomment next line to draw the board */
+  // await renderer()(state)
 
   return state.filter((tile) => tile[2] === 2).length
 }
@@ -21,8 +55,9 @@ const goB = async (source: string) => {
   let score = 0
   let paddleX = 0
   let ballX = 0
+  const draw = renderer()
 
-  while (!done) {
+  do {
     await unblock()
 
     const out = []
@@ -41,10 +76,13 @@ const goB = async (source: string) => {
     paddleX = paddle ? paddle[0] : paddleX
     score = currentScore ? currentScore[2] : score
 
+    /* Uncomment next line to draw the board */
+    await draw(state, score)
+
     const move = paddleX < ballX ? 1 : paddleX > ballX ? -1 : 0
     paddleX += move
     inputs.push(move)
-  }
+  } while (!done)
 
   return score
 }
