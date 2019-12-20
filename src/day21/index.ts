393b1022412b298f85a37ff07d4296cf80801326
Maciej Cąderek <maciej.caderek@gmail.com>
2019-12-20 23:42:28 +0100

    Cleanup

---
+++ src/day21/index.ts
@@ -0,0 +1,47 @@
+import {
+  test,
+  readInput,
+  arr,
+  com,
+  mul,
+  dis,
+  math,
+  iter,
+  R,
+  graph,
+  log,
+} from "../utils/index"
+
+const prepareInput = (rawInput: string) => rawInput
+
+const goA = (rawInput: string) => {
+  const input = prepareInput(rawInput)
+
+  return
+}
+
+const goB = (rawInput: string) => {
+  const input = prepareInput(rawInput)
+
+  return
+}
+
+/* Tests */
+
+// test(goA(""), )
+// test(goA(""), )
+
+// test(goB(""), )
+// test(goB(""), )
+
+/* Results */
+
+const input = readInput()
+
+console.time("Time")
+const resultA = goA(input)
+const resultB = goB(input)
+console.timeEnd("Time")
+
+console.log("Solution to part 1:", resultA)
+console.log("Solution to part 2:", resultB)
