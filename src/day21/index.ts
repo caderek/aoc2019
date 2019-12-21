723a2a84f2b2d6c9b11088cc8d66a90577e1bdb3
Maciej Cąderek <maciej.caderek@gmail.com>
2019-12-21 21:17:57 +0100

    Day 21 solution

--- src/day21/index.ts
+++ src/day21/index.ts
@@ -1,47 +1,70 @@
-import {
-  test,
-  readInput,
-  arr,
-  com,
-  mul,
-  dis,
-  math,
-  iter,
-  R,
-  graph,
-  log,
-} from "../utils/index"
+import { readInput, arr } from "../utils/index"
+import compute from "./computer"
 
-const prepareInput = (rawInput: string) => rawInput
+const toASCII = (instructions: string) =>
+  instructions
+    .trim()
+    .split("\n")
+    .map((instruction) =>
+      instruction
+        .replace(/ \/\/.*/, "")
+        .trim()
+        .split("")
+        .concat("\n")
+        .map((char) => char.charCodeAt(0)),
+    )
+    .flat()
 
-const goA = (rawInput: string) => {
-  const input = prepareInput(rawInput)
+const goA = async (source: string) => {
+  const program = `
+    NOT C J
+    OR D T
+    AND T J
+    NOT A T
+    OR T J
+    WALK
+  `
 
-  return
+  const output = await compute(source, toASCII(program))
+
+  console.log(String.fromCharCode(...arr.butLast_(output)))
+
+  return arr.last_(output)
 }
 
-const goB = (rawInput: string) => {
-  const input = prepareInput(rawInput)
+const goB = async (source: string) => {
+  const program = `
+    NOT C T
+    NOT A J
+    AND H T
+    OR T J
+    NOT B T
+    AND C T
+    AND A T
+    OR T J
+    AND D J
+    RUN
+  `
 
-  return
+  const output = await compute(source, toASCII(program))
+
+  console.log(String.fromCharCode(...arr.butLast_(output)))
+
+  return arr.last_(output)
 }
 
-/* Tests */
+const main = async () => {
+  /* Results */
 
-// test(goA(""), )
-// test(goA(""), )
+  const input = readInput()
 
-// test(goB(""), )
-// test(goB(""), )
+  console.time("Time")
+  const resultA = await goA(input)
+  const resultB = await goB(input)
+  console.timeEnd("Time")
 
-/* Results */
+  console.log("Solution to part 1:", resultA) // -> 19352720
+  console.log("Solution to part 2:", resultB) // -> 1143652885
+}
 
-const input = readInput()
-
-console.time("Time")
-const resultA = goA(input)
-const resultB = goB(input)
-console.timeEnd("Time")
-
-console.log("Solution to part 1:", resultA)
-console.log("Solution to part 2:", resultB)
+main()
