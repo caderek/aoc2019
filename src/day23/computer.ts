591e4df551afe3c0744d3ec540a8185125ecd321
Maciej Cąderek <maciej.caderek@gmail.com>
2019-12-23 07:11:02 +0100

    Day 23 solution

---
+++ src/day23/computer.ts
@@ -0,0 +1,166 @@
+enum Jumps {
+  ADD = 4,
+  MULTIPLY = 4,
+  INPUT = 2,
+  OUTPUT = 2,
+  JUMP_IF_TRUE = 3,
+  JUMP_IF_FALSE = 3,
+  LESS_THAN = 4,
+  EQUALS = 4,
+  RELATIVE_BASE_OFFSET = 2,
+}
+
+enum Ops {
+  ADD = 1,
+  MULTIPLY,
+  INPUT,
+  OUTPUT,
+  JUMP_IF_TRUE,
+  JUMP_IF_FALSE,
+  LESS_THAN,
+  EQUALS,
+  RELATIVE_BASE_OFFSET,
+  HALT = 99,
+}
+
+enum Modes {
+  POSITION,
+  IMMEDIATE,
+  RELATIVE,
+}
+
+type Program = number[]
+
+const unblock = () => new Promise(setImmediate)
+
+const calculations: { [key: string]: (a: number, b: number) => number } = {
+  [Ops.ADD]: (a, b) => a + b,
+  [Ops.MULTIPLY]: (a, b) => a * b,
+  [Ops.LESS_THAN]: (a, b) => Number(a < b),
+  [Ops.EQUALS]: (a, b) => Number(a === b),
+}
+
+const getCell = (index: number, program: Program) => {
+  if (!program[index]) {
+    program[index] = 0
+    return 0
+  }
+
+  return program[index]
+}
+
+const setCell = (index: number, value: number, program: Program) => {
+  program[index] = value
+}
+
+const getValue = (
+  program: Program,
+  params: number[],
+  pointer: number,
+  index: number,
+  relativeBase: number,
+): number => {
+  return getCell(
+    params[index] === Modes.POSITION
+      ? getCell(pointer + index + 1, program)
+      : params[index] === Modes.IMMEDIATE
+      ? pointer + index + 1
+      : relativeBase + getCell(pointer + index + 1, program),
+    program,
+  )
+}
+
+const getWriteIndex = (
+  program: Program,
+  params: number[],
+  pointer: number,
+  relativeBase: number,
+) => (index: number) => {
+  return params[index] === Modes.RELATIVE
+    ? relativeBase + getCell(pointer + index + 1, program)
+    : getCell(pointer + index + 1, program)
+}
+
+const compute = async (
+  source: string,
+  inputs: number[] = [],
+  outputs: number[] = [],
+  phaseSettings: number[] = [],
+) => {
+  const program = source.split(",").map(Number)
+
+  let pointer = 0
+  let relativeBase = 0
+
+  while (true) {
+    const first = String(getCell(pointer, program)).padStart(5, "0")
+
+    const opcode = Number(first.substr(3))
+
+    if (opcode === Ops.HALT) {
+      break
+    }
+
+    const params = first
+      .substr(0, 3)
+      .split("")
+      .reverse()
+      .map(Number)
+
+    let shouldJump = true
+
+    const a: number = getValue(program, params, pointer, 0, relativeBase)
+    const b: number = getValue(program, params, pointer, 1, relativeBase)
+
+    const getIndex = getWriteIndex(program, params, pointer, relativeBase)
+
+    switch (opcode) {
+      case Ops.ADD:
+      case Ops.MULTIPLY:
+      case Ops.LESS_THAN:
+      case Ops.EQUALS: {
+        setCell(getIndex(2), calculations[opcode](a, b), program)
+        break
+      }
+      case Ops.INPUT: {
+        if (phaseSettings.length > 0) {
+          setCell(getIndex(0), phaseSettings.shift(), program)
+        } else if (inputs.length > 0) {
+          setCell(getIndex(0), inputs.shift(), program)
+        } else {
+          setCell(getIndex(0), -1, program)
+          await unblock()
+          // shouldJump = false
+        }
+        break
+      }
+      case Ops.OUTPUT: {
+        outputs.push(a)
+        break
+      }
+      case Ops.JUMP_IF_TRUE: {
+        pointer = a !== 0 ? b : pointer
+        shouldJump = a === 0
+        break
+      }
+      case Ops.JUMP_IF_FALSE: {
+        pointer = a === 0 ? b : pointer
+        shouldJump = a !== 0
+        break
+      }
+      case Ops.RELATIVE_BASE_OFFSET: {
+        relativeBase += a
+        break
+      }
+    }
+
+    if (shouldJump) {
+      pointer += Jumps[Ops[opcode]]
+    }
+  }
+
+  return outputs
+}
+
+export { unblock }
+export default compute
---
+++ src/day23/index.ts
@@ -0,0 +1,95 @@
+import { readInput, arr } from "../utils/index"
+import compute, { unblock } from "./computer"
+
+const goA = async (source: string) => {
+  const inputBuffers = Array.from({ length: 50 }, () => [])
+  const outputBuffers = Array.from({ length: 50 }, () => [])
+
+  inputBuffers.forEach((inputs, i) =>
+    compute(source, inputs, outputBuffers[i], [i]),
+  )
+
+  while (true) {
+    await unblock()
+    const packets = outputBuffers.map((buffer) => arr.chunk_(3, buffer)).flat()
+
+    const address255 = packets.find((x) => x[0] === 255)
+
+    if (address255 !== undefined) {
+      return address255[2]
+    }
+
+    outputBuffers.forEach((buffer) => {
+      const out = []
+      while (buffer.length > 0) {
+        out.push(buffer.shift())
+      }
+
+      arr.chunk_(3, out).forEach(([address, X, Y]) => {
+        inputBuffers[address].push(X)
+        inputBuffers[address].push(Y)
+      })
+    })
+  }
+}
+
+const goB = async (source: string) => {
+  const inputBuffers = Array.from({ length: 50 }, () => [])
+  const outputBuffers = Array.from({ length: 50 }, () => [])
+
+  inputBuffers.forEach((inputs, i) =>
+    compute(source, inputs, outputBuffers[i], [i]),
+  )
+
+  let prevNat = [-1, -1]
+  let nat = []
+
+  while (true) {
+    await unblock()
+
+    if (
+      inputBuffers.every((x) => x.length === 0) &&
+      outputBuffers.every((x) => x.length === 0)
+    ) {
+      inputBuffers[0].push(nat[0])
+      inputBuffers[0].push(nat[1])
+    }
+
+    if (nat[1] === prevNat[1]) {
+      return nat[1]
+    }
+
+    outputBuffers.forEach((buffer) => {
+      const out = []
+      while (buffer.length > 0) {
+        out.push(buffer.shift())
+      }
+
+      arr.chunk_(3, out).forEach(([address, X, Y]) => {
+        if (address === 255) {
+          prevNat = nat
+          nat = [X, Y]
+        } else {
+          inputBuffers[address].push(X)
+          inputBuffers[address].push(Y)
+        }
+      })
+    })
+  }
+}
+
+/* Results */
+
+const main = async () => {
+  const input = readInput()
+
+  console.time("Time")
+  const resultA = await goA(input)
+  const resultB = await goB(input)
+  console.timeEnd("Time")
+
+  console.log("Solution to part 1:", resultA) // -> 17541
+  console.log("Solution to part 2:", resultB) // -> 12415
+}
+
+main()
